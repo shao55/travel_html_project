676771066e9d5c3b532aa77b6bf0e363ae65f0f8
Nurgissa <nurgissa.22@gmail.com>
2022-08-01 21:16:00 +0600

    Day 6

--- js/main.js
+++ js/main.js
@@ -19,8 +19,11 @@ $('.slider-btn--next').click(function() {
 // Кнопка навигации
 const nav_btn = document.querySelector(".nav_toggle");
 const menu_icon = document.querySelector(".menu-icon");
+const nav = document.querySelector(".nav");
 // При клике на объект с классом .nav_toggle
 nav_btn.onclick = function(){
-    // Запускается функция с тогглом menu-icon-active
+    // Запускается функция с тогглом, добавляющий удаляющий класс
     menu_icon.classList.toggle('menu-icon-active');
+    nav.classList.toggle("nav_mobile");
+    document.body.classList.toggle("no-scroll");
 };
