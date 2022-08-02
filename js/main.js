ff45395c66e596d200b9d086d5046e2ae8bf4672
Nurgissa <nurgissa.22@gmail.com>
2022-08-02 15:49:22 +0600

    Final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-// Карусель
+// Carousel
 const owl = $('.owl-carousel');
+
 owl.owlCarousel({
 	center: true,
 	loop: true,
@@ -18,22 +19,23 @@ owl.owlCarousel({
 	},
 });
 
-$('.slider-btn--prev').click(function() {
-    owl.trigger('prev.owl.carousel');
+$('.slider__btn--prev').click(function () {
+	owl.trigger('prev.owl.carousel');
 });
 
-$('.slider-btn--next').click(function() {
-    owl.trigger('next.owl.carousel');
+
+$('.slider__btn--next').click(function () {
+	owl.trigger('next.owl.carousel');
 });
 
-// Кнопка навигации
-const nav_btn = document.querySelector(".nav_toggle");
-const menu_icon = document.querySelector(".menu-icon");
-const nav = document.querySelector(".nav");
-// При клике на объект с классом .nav_toggle
-nav_btn.onclick = function(){
-    // Запускается функция с тогглом, добавляющий удаляющий класс
-    menu_icon.classList.toggle('menu-icon-active');
-    nav.classList.toggle("nav_mobile");
-    document.body.classList.toggle("no-scroll");
+// Nav icon
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav')
+const menuIcon = document.querySelector('.menu-icon');
+
+
+navBtn.onclick = function () {
+	nav.classList.toggle('nav--mobile')
+	menuIcon.classList.toggle('menu-icon--active');
+	document.body.classList.toggle('no-scroll');
 };
